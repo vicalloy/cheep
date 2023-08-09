26e5bfd9cf33aa1552dfa716006eb814ec4dc9a3
vicalloy <zbirder@gmail.com>
2023-08-09 14:38:00 +0800

    add latest_tv_watch_time

--- cheep/server.py
+++ cheep/server.py
@@ -1,18 +1,17 @@
 import asyncio
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from cheep.device import Device, get_on_device
 from cheep.utils import speak, turn_off_plug
 
 
-async def turn_off_plug_if_needed(passed_time: int, device: Device) -> bool:
+def is_parents_in_home():
     now = datetime.now()
-    if (
-        device != Device.WII_U
-        or passed_time <= 25
-        or now.hour >= 19
-        or now.weekday() >= 5
-    ):
+    return now.hour >= 19 or now.weekday() >= 5
+
+
+async def turn_off_plug_if_needed(passed_time: int, device: Device) -> bool:
+    if device != Device.WII_U or passed_time <= 25 or is_parents_in_home():
         return False
     await speak("将在2分钟后关闭插座")
     await asyncio.sleep(2 * 60)
@@ -36,24 +35,38 @@ async def device_alert(passed_time: int, waiting_time: int) -> int:
     return passed_time
 
 
-async def count_down(device: Device):
+async def count_down(device: Device, latest_tv_watch_time: datetime) -> timedelta:
+    def end():
+        print(
+            f"{device.name} closed on {datetime.now().strftime('%Y %H:%M')},"
+            f"{datetime.now() - started_on}"
+        )
+        return datetime.now() - started_on
+
+    if (datetime.now() - latest_tv_watch_time) < timedelta(
+        minutes=30
+    ) and not is_parents_in_home():
+        await speak("请先休息30分钟眼睛后再看电视")
+        await turn_off_plug()
+        return timedelta()
+
     await speak(f"{device.value}开了")
     started_on = datetime.now()
     print(f"{device.name} opened on {started_on.strftime('%Y %H:%M')}")
     passed_time = 0
     if (passed_time := await device_alert(passed_time, 15)) == 0:
-        return
+        return end()
     while passed_time := await device_alert(passed_time, 5):
         pass
-    print(
-        f"{device.name} closed on {datetime.now().strftime('%Y %H:%M')},"
-        f"{datetime.now() - started_on}"
-    )
+    return end()
 
 
 async def main():
+    latest_tv_watch_time = datetime.now()
     while True:
         device = await get_on_device()
-        if device:
-            await count_down(device)
+        if device and (
+            await count_down(device, latest_tv_watch_time)
+        ).total_seconds() > (60 * 20):
+            latest_tv_watch_time = datetime.now()
         await asyncio.sleep(60)
